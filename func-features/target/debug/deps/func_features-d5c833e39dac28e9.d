a721ca99ebc7126afe29f168d480af9f1af540bd
Frank Jansen <fphjansen@gmail.com>
2023-12-20 11:26:28 -0500

    Noodling with closures

---
+++ func-features/target/debug/deps/func_features-d5c833e39dac28e9.d
@@ -0,0 +1,5 @@
+/Users/fpj/Development/rust/rustling/func-features/target/debug/deps/func_features-d5c833e39dac28e9: src/main.rs
+
+/Users/fpj/Development/rust/rustling/func-features/target/debug/deps/func_features-d5c833e39dac28e9.d: src/main.rs
+
+src/main.rs:
---
+++ func-features/target/debug/func-features.d
@@ -0,0 +1,1 @@
+/Users/fpj/Development/rust/rustling/func-features/target/debug/func-features: /Users/fpj/Development/rust/rustling/func-features/src/main.rs
